42250269f378427838454f06abad91cb6f9814f3
A <aboyle5@hotmail.com>
2016-04-02 12:47:54 -0700

    add comments to get-wow

--- js/get-wow.js
+++ js/get-wow.js
@@ -34,15 +34,21 @@ function doit() {
     cookie = getCookie('pyladiespdx_wow');
     if (cookie === 'yes'){
         console.log("Adding WOW to site style!");
+        // add css and js
         $('head').append( $('<link rel="stylesheet" type="text/css" />').attr('href', '/css/geor.css') );
         $('body').append( $('<script src="https://bloggerxtutorials.googlecode.com/files/pinksparkle-min.js" type="text/javascript"></script>') );
-        $('#content-container').append( '<iframe width="0" height="0" src="https://www.youtube.com/embed/gJLIiF15wjQ?rel=0&amp;controls=0&amp;showinfo=0&amp;start=49&amp;autoplay=1" frameborder="0" allowfullscreen></iframe>' );
 
+        // Make existing components "pop" more
+        $('#content-container').append( '<iframe width="0" height="0" src="https://www.youtube.com/embed/gJLIiF15wjQ?rel=0&amp;controls=0&amp;showinfo=0&amp;start=49&amp;autoplay=1" frameborder="0" allowfullscreen></iframe>' );
+        $('#main-img').replaceWith('<img id="main-img" class="img-responsive" src="/images/pyladies_pdx_wow.png" alt="pyladies pdx logo" />');
         title = '<h1 id="pyladies-title">Welcome to <FONT COLOR="#FF0000">P</FONT><FONT COLOR="#FF5A00">y</FONT><FONT COLOR="#FFB400">L</FONT><FONT COLOR="#FFff00">a</FONT><FONT COLOR="#A5ff00">d</FONT><FONT COLOR="#4Bff00">i</FONT><FONT COLOR="#00ff00">e</FONT><FONT COLOR="#00ff5A">s</FONT> <blink><FONT COLOR="#00ffB4">P</FONT><FONT COLOR="#00ffff">D</FONT><FONT COLOR="#00B4ff">X</FONT> !!!</blink></h1>';
         $('#pyladies-title').replaceWith(title);
+
+        // add silly hampsters
         $('#potential-hampsters').replaceWith('<img src="/images/hampster2.gif"><img src="/images/hampster1.gif"><img src="/images/hampster3.gif">');
         $('#potential-gerbil').replaceWith('<img src="/images/gerbil.gif">');
-        $('#main-img').replaceWith('<img id="main-img" class="img-responsive" src="/images/pyladies_pdx_wow.png" alt="pyladies pdx logo" />');
+
+        // Add ability to remove wow -- why would anyone do this???? Georgia???
         $('#less-wow').replaceWith('<a id="less-wow" href="#">Click here for less WOW</a>');
         $('#less-wow').click(function () { 
             removeCookie("pyladiespdx_wow"); 
